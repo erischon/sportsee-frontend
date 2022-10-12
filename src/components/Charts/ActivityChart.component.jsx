0a729800b4ca148640c99c55986ff3384c9cc4ba
Eri Schon <erischon@gmail.com>
2022-10-12 10:09:43 +0200

    Update ActivityChart.component.jsx

--- src/components/Charts/ActivityChart.component.jsx
+++ src/components/Charts/ActivityChart.component.jsx
@@ -1,21 +1,105 @@
-import { ResponsiveContainer, BarChart, Bar } from "recharts";
+import React, { PureComponent } from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-import { getUserActivity } from "../../utils/services";
+export default class ActivityChart extends PureComponent {
+  static demoUrl =
+    "https://codesandbox.io/s/bar-chart-with-customized-event-4k1bd";
 
-const ActivityChart = () => {
-  const userActivity = getUserActivity(12);
-  const sessions = userActivity.sessions;
+  state = {
+    data: [
+      {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+      },
+      {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+      },
+      {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+      },
+      {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+      },
+      {
+        name: "Page E",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+      },
+      {
+        name: "Page F",
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+      },
+      {
+        name: "Page G",
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      },
+    ],
+    activeIndex: 0,
+  };
 
-  console.log("======USERACTIVITY", sessions);
+  handleClick = (data, index) => {
+    this.setState({
+      activeIndex: index,
+    });
+  };
 
-  return (
-    <div>
-      <BarChart width={150} height={40} data={sessions}>
-        <Bar dataKey="kilogram" fill="#282D30" />
-        <Bar dataKey="calories" fill="#E60000" />
-      </BarChart>
-    </div>
-  );
-};
+  render() {
+    const { activeIndex, data } = this.state;
+    const activeItem = data[activeIndex];
 
-export default ActivityChart;
+    return (
+      <div style={{ width: "100%" }}>
+        <p>Click each rectangle </p>
+        <ResponsiveContainer width="100%" height={100}>
+          <BarChart width={150} height={40} data={data}>
+            <Bar dataKey="uv" onClick={this.handleClick}>
+              {data.map((entry, index) => (
+                <Cell
+                  cursor="pointer"
+                  fill={index === activeIndex ? "#82ca9d" : "#8884d8"}
+                  key={`cell-${index}`}
+                />
+              ))}
+            </Bar>
+            <Bar dataKey="pv" onClick={this.handleClick}>
+              {data.map((entry, index) => (
+                <Cell
+                  cursor="pointer"
+                  fill={index === activeIndex ? "#82ca9d" : "#8884d8"}
+                  key={`cell-${index}`}
+                />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+        <p className="content">{`Uv of "${activeItem.name}": ${activeItem.uv}`}</p>
+      </div>
+    );
+  }
+}
