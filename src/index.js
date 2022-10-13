1c08bd76cbb2d23d67e6ad2dfa95c2370c7621ec
Eri Schon <erischon@gmail.com>
2022-10-13 10:14:08 +0200

    sass setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
+// import "./sass/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
