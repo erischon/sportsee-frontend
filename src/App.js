e0d6e38a77914f19ae14f6ea02ff2ab9c65a7ab4
Eri Schon <erischon@gmail.com>
2022-10-14 11:28:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout.component";
 import Dashboard from "./pages/Dashboard.page";
 import NotFound from "./pages/NotFound.page";
+import Service from "./utils/service2";
 
 function App() {
+  let data = null;
+
+  (async () => {
+    data = await Service();
+  })();
+  console.log("======APP", data);
+
   return (
     <Routes>
       {/* Public Routes */}
