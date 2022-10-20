ffa582a3b7cff0955b5d0251ec9503e6e0ac79ea
Eri Schon <erischon@gmail.com>
2022-10-20 10:27:28 +0200

    KeyInfos component

--- src/pages/Dashboard.page.jsx
+++ src/pages/Dashboard.page.jsx
@@ -2,10 +2,12 @@ import ActivityChart from "../components/Charts/ActivityChart.component";
 import ActivityTypeChart from "../components/Charts/ActivityTypeChart.component";
 import AverageScoreChart from "../components/Charts/AverageScoreChart.component";
 import AverageSessionsChart from "../components/Charts/AverageSessionsChart.component";
+import KeyInfos from "../components/KeyInfos/KeyInfos.component";
 
 const Dashboard = () => {
   return (
     <main>
+      <KeyInfos />
       <AverageScoreChart />
       <ActivityTypeChart />
       <AverageSessionsChart />
