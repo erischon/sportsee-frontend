4d83d87de3b9c98939f31feec53845703ba15430
Eri Schon <erischon@gmail.com>
2022-11-04 10:40:32 +0100

    documentation for menu components

--- src/components/menu/MenuCard.component.jsx
+++ src/components/menu/MenuCard.component.jsx
@@ -1,3 +1,11 @@
+/**
+ * Component who create Menu Card
+ * @component
+ * @param {Object} props
+ * @param {{color: string}} props.color
+ * @param {JSX.Element} props.children
+ * @returns {React.ReactElement}
+ */
 const MenuCard = ({ color, children }) => {
   return (
     <div className="menu-card">
--- src/components/menu/VerticalMenu.component.jsx
+++ src/components/menu/VerticalMenu.component.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+
 import MenuCard from "./MenuCard.component";
 import { Menu1, Menu2, Menu3, Menu4 } from "./MenuIcons";
 
