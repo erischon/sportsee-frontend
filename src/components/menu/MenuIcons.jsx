00ecd65f2e19c684f10dc0add978b5cc3fff1e7f
Eri Schon <erischon@gmail.com>
2022-11-17 10:09:28 +0100

    Update MenuIcons.jsx

--- src/components/menu/MenuIcons.jsx
+++ src/components/menu/MenuIcons.jsx
@@ -3,6 +3,8 @@ import menu2 from "../../assets/icons/menu2.svg";
 import menu3 from "../../assets/icons/menu3.svg";
 import menu4 from "../../assets/icons/menu4.svg";
 
+/** Created several Component for Icons */
+
 export const Menu1 = () => {
   return (
     <>
